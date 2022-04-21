477bc8bcd4bfcb5eba84945332ba4afaf81ad665
SIDDHARTH BAJ <sidbaj25>
2022-04-21 08:43:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
@@ -6,29 +6,24 @@ import GridComponents from "./Components";
 import Modal from "react-modal";
 import uuid from "uuidv4";
 
+
 Modal.setAppElement("#root");
 
-const frameworkComponents = {
-    actionsRenderer: GridComponents.ActionsRenderer
-};
+// const frameworkComponents = {
+//     athleteRender: athleteRender
+// };
 
 const App = () => {
 
     const [rowData, setRowData] = useState([]);
-
     const [isOpen, setIsOpen] = useState(false);
+    const [gridApi, setGridApi] = React.useState(() => { });
 
-    function toggleModal() {
-        setIsOpen(!isOpen);
-    }
-
-    const customStyle={
-        content:{
-            width:'50%',
-            height:260,
-            margin:'auto'
-        }
-    }
+    const [validAthlete, setValidAthelte] = useState(true);
+    const [validGold, setValidGold] = useState(true);
+    const [validSilver, setValidSilver] = useState(true);
+    const [validBronze, setValidBronze] = useState(true);
+    const [validCountery, setValidCountry] = useState(true);
 
     const [athlete, setAthlete] = useState('');
     const [gold, setGold] = useState('');
@@ -37,21 +32,42 @@ const App = () => {
     const [country, setCountry] = useState('');
 
     const [columnDefs] = useState([
-        { field: "athlete", sortable: true, filter: true, editable: true },
+        { field: "athlete", sortable: true, filter: true, editable: true, checkboxSelection: true },
         { field: "gold", sortable: true, filter: true, editable: true },
         { field: "silver", sortable: true, filter: true, editable: true },
         { field: "bronze", sortable: true, filter: true, editable: true },
-        { field: "country", sortable: true, filter: true, editable: true },
-        {
-            headerName: "",
-            colId: "actions",
-            cellRenderer: "actionsRenderer",
-            editable: false,
-            filter: false,
-            minWidth: 220
-        }]);
+        { field: "country", sortable: true, filter: true, editable: true }
+        // {
+        //     headerName: "",
+        //     colId: "actions",
+        //     cellRenderer: "actionsRenderer",
+        //     editable: false,
+        //     filter: false,
+        //     minWidth: 220
+        // }
+    ]);
 
-    const onGridReady = (params) => {
+    function toggleModal() {
+        setIsOpen(!isOpen);
+    }
+
+    const onCellValueChanged = ({ data }) => {
+        // console.log(data);
+        if (data.athlete === '' || data.gold === '' || data.silver === '' || data.bronze === '' || data.country === '') {
+            window.confirm(`Please fill all details`);
+            dataFetch();
+        }
+        else {
+
+        }
+    }
+
+
+    const onGridReady = params => {
+        setGridApi(params.api);
+    };
+
+    const dataFetch = () => {
         fetch(
             "https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/olympicWinnersSmall.json"
         )
@@ -60,11 +76,12 @@ const App = () => {
                 data.forEach(row => (row.id = uuid()));
                 console.log(data.slice(0, 100));
                 setRowData(data.slice(0, 100));
-            });
+            })
+
     };
-    
+
     useEffect(() => {
-        onGridReady();
+        dataFetch();
     }, []);
 
     const addNewRow = () => {
@@ -77,41 +94,82 @@ const App = () => {
             id: uuid()
         }
 
+        if (athlete === '') {
+            setValidAthelte(false);
+            return window.confirm(`Please fill Athlete name`);
+        }
+        if (gold === '') {
+            setValidGold(false);
+            return window.confirm(`Please fill No of Gold medals`);
+        }
+        if (silver === '') {
+            setValidSilver(false);
+            return window.confirm(`Please fill No of Silver medals`);
+        }
+        if (bronze === '') {
+            setValidBronze(false);
+            return window.confirm(`Please fill No of Bronze medals`);
+        }
+        if (country === '') {
+            setValidCountry(false);
+            return window.confirm(`Please fill Country name`);
+        }
+
         setRowData((prev) => ([
             ...prev,
             newRow
         ]))
         toggleModal()
+        setAthlete('');
+        setGold('');
+        setSilver('');
+        setBronze('');
+        setCountry('');
+
+    }
+
+    const deleteRow = () => {
+        const selectedRows = gridApi.getSelectedRows();
+        console.log("selectedRow", selectedRows);
+        setRowData(rowData.filter
+            (row => {
+                return selectedRows.indexOf(row) == -1
+            }))
     }
 
     return (
         <div className="ag-theme-alpine" style={{ height: 600, width: 1250 }}>
-            <button onClick={toggleModal} style={{marginLeft:'600px'}}>Add Row</button>
+            <button onClick={toggleModal}>Open modal</button>
+            <button
+                color="secondary"
+                variant="outlined"
+                data-action="delete"
+                onClick={() => deleteRow()}>Delete</button>
             <AgGridReact
                 rowData={rowData}
                 onGridReady={onGridReady}
                 columnDefs={columnDefs}
-                frameworkComponents={frameworkComponents}
-                getRowNodeId={data => data.id}>
+                rowSelection={'multiple'}
+                getRowNodeId={data => data.id}
+                onCellValueChanged={onCellValueChanged}>
             </AgGridReact>
             <Modal
                 isOpen={isOpen}
                 onRequestClose={toggleModal}
                 contentLabel="My dialog"
-                style={customStyle}
             >
                 <div>
-                    <form style={{ width: '50px' }}>
+                    <form>
                         <label for="athlete">Athlete:</label>
                         <input onChange={(e) => setAthlete(e.target.value)} type="text" id="athlete" name="athlete"></input>
                         <label for="gold">Gold:</label>
-                        <input onChange={(e)=> setGold(e.target.value)} type="text" id="gold" name="gold"></input>
+                        <input onChange={(e) => setGold(e.target.value)} type="text" id="gold" name="gold"></input>
                         <label for="silver">Silver:</label>
-                        <input onChange={(e)=> setSilver(e.target.value)} type="text" id="silver" name="silver"></input>
+                        <input onChange={(e) => setSilver(e.target.value)} type="text" id="silver" name="silver"></input>
                         <label for="bronze">Bronze:</label>
-                        <input onChange={(e)=> setBronze(e.target.value)} type="text" id="bronze" name="bronze"></input>
+                        <input onChange={(e) => setBronze(e.target.value)} type="text" id="bronze" name="bronze"></input>
                         <label for="country">Country:</label>
-                        <input onChange={(e)=> setCountry(e.target.value)}type="text" id="country" name="country"></input>
+                        <input onChange={(e) => setCountry(e.target.value)} type="text" id="country" name="country"></input>
 
                     </form>
                     <button onClick={addNewRow} >save</button>
